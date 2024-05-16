f0358278ed8082539a6695628abd823787b54678
ishtihoss <ishtiaque.hoss89@gmail.com>
2024-05-16 16:02:01 -0400

    got rid of empty white header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -30,11 +30,6 @@ export default function App({ Component, pageProps }: AppProps<{}>) {
         }
       `}</style>
       <div className="min-h-screen bg-gray-100">
-        <header className="bg-white shadow">
-          <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-            
-          </div>
-        </header>
         <main className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
           <Component {...pageProps} />
         </main>
