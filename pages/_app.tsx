335c25d5f1105c4135d37b8f69dc0a822da73568
ishtihoss <ishtiaque.hoss89@gmail.com>
2024-05-16 11:35:27 -0400

    moved more header text

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -32,7 +32,7 @@ export default function App({ Component, pageProps }: AppProps<{}>) {
       <div className="min-h-screen bg-gray-100">
         <header className="bg-white shadow">
           <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold text-gray-900">Express Entry Search Engine</h1>
+            
           </div>
         </header>
         <main className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
