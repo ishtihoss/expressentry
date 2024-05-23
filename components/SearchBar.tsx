64cfd3134a305471be9c4df0b2d503768f185a49
ishtihoss <dexclaw@gmail.com>
2024-05-22 23:53:36 -0400

    cool search checkpoint

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,6 +1,7 @@
 // components/SearchBar.tsx
 import { IconSearch } from "@tabler/icons-react";
 import { KeyboardEvent, useState } from "react";
+import styles from "./searchbar.module.css";
 
 interface SearchBarProps {
   onSearch: (query: string) => void;
@@ -8,18 +9,24 @@ interface SearchBarProps {
 
 export const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
   const [query, setQuery] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+    setIsLoading(true);
     try {
       await fetch("/api/save-query", {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
+        headers: {
+          "Content-Type": "application/json",
+        },
         body: JSON.stringify({ query }),
       });
       onSearch(query);
     } catch (error) {
       console.error("Error saving query:", error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -30,21 +37,29 @@ export const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="relative">
+    <form onSubmit={handleSubmit} className={styles.searchForm}>
       <input
-        className="w-full pl-12 pr-20 py-3 text-lg text-gray-800 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+        className={styles.searchInput}
         type="text"
         placeholder="Ask a question about Express Entry..."
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         onKeyDown={handleKeyDown}
       />
-      <button
-        type="submit"
-        className="absolute right-2 top-2 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
-      >
-        <IconSearch className="h-6 w-6" />
+      <button type="submit" className={styles.searchButton}>
+        <IconSearch className={styles.searchIcon} />
       </button>
+      {isLoading && (
+        <div className={styles.equalizerContainer}>
+          <div className={styles.equalizer}>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+      )}
     </form>
   );
 };
