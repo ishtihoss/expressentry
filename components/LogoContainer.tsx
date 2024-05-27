ee5391691f90ef5fe0e6c6134fd0f7a71513f033
ishtihoss <dexclaw@gmail.com>
2024-05-27 17:34:09 -0400

    changed logo to alarm animation

--- components/LogoContainer.tsx
+++ components/LogoContainer.tsx
@@ -1,18 +1,15 @@
 // components/LogoContainer.tsx
 import Image from "next/image";
+import styles from "./logoContainer.module.css";
 
 export const LogoContainer = () => {
   return (
     <div className="relative inline-block">
       <div className="absolute inset-0 bg-gradient-to-br from-blue-500 to-green-500 rounded-lg transform rotate-3 shadow-lg"></div>
       <div className="relative bg-white p-6 rounded-lg shadow-md">
-        <Image
-          src="/logo.png"
-          alt="Express Entry Search Engine Logo"
-          width={150}
-          height={150}
-          className="mx-auto"
-        />
+        <div className={`${styles.container} mx-auto`}>
+          <div className={styles.sprite}></div>
+        </div>
       </div>
     </div>
   );
